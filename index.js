f3a20162116c27fdfb9137fc913737783b12c668
anki356 <anki356@gmail.com>
2023-05-11 14:03:38 +0530

    push env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const http=require('http')
-const app=require("./app/app")
-const server=http.createServer(app)
-const port = process.env.PORT || 5000;
+const http = require('http')
+const app = require("./app/app")
+const server = http.createServer(app)
+const port = process.env.PORT || 9000;
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});

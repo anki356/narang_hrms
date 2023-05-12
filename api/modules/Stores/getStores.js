70e6c7756e267328385a8a749a62511f074816f4
wdAres <puneetsri9990@gmail.com>
2023-05-12 11:36:27 +0530

    ..

--- api/modules/Stores/getStores.js
+++ api/modules/Stores/getStores.js
@@ -1,14 +1,10 @@
 const database = require("../../../config/database");
 const mysql = require("mysql")
 const getStores = (req, res, next) => {
-    
             database.query("select * from stores", (err, storeData, fields) => {
                 console.log(err)
                 res.send(storeData) 
-                    
             })
-       
-
 }
 
 module.exports=getStores

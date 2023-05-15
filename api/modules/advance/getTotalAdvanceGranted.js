715fcf811cbdc33ea2fca27d3543899d8a1a1af6
anki356 <anki356@gmail.com>
2023-05-15 17:42:10 +0530

    Some Changes

--- api/modules/advance/getTotalAdvanceGranted.js
+++ api/modules/advance/getTotalAdvanceGranted.js
@@ -1,14 +1,14 @@
 const database = require("../../../config/database");
 const mysql=require('mysql')
-const getTotalEmployeesTakenLoan = (req, res, next) => {
+const getTotalAdvanceGranted = (req, res, next) => {
     const role_id = req.body.result.role_id
     database.query("Select * from roles where id=" + role_id, (err, result) => {
         if (err) console.log(err)
         let allowed_roles = ['HR Head','Admin','Super Admin']
         if (allowed_roles.includes(result[0].role_name)) {
-            database.query("SELECT count(distinct employee_id ) as count_id from loan where date>="+mysql.escape(req.query.from_date)+" and date<"+mysql.escape(req.query.to_date)+" and status='Approved'" , (err, loanData, fields) => {
+            database.query("SELECT sum(amount) as amount from advance where date>="+mysql.escape(req.query.from_date)+" and date<"+mysql.escape(req.query.to_date)+" and status='Approved'" , (err, advanceData, fields) => {
                 console.log(err)
-                res.send(loanData) 
+                res.send(advanceData) 
                     
             })
         }
@@ -18,4 +18,4 @@ const getTotalEmployeesTakenLoan = (req, res, next) => {
     })
 
 }
-module.exports=getTotalEmployeesTakenLoan
+module.exports=getTotalAdvanceGranted
---
+++ api/modules/leaves/getTotalLeaves.js
@@ -0,0 +1,25 @@
+const database = require("../../../config/database");
+const mysql = require("mysql")
+const getTotalLeaves = (req, res, next) => {
+    const role_id = req.body.result.role_id
+    database.query("Select * from roles where id=" + role_id, (err, result) => {
+        if (err) console.log(err)
+        let allowed_roles = ['HR Head','Super Admin','Admin']
+        if (allowed_roles.includes(result[0].role_name)) {
+            let queryString="SELECT count (id) as count_id from leaves where((leaves.from_date between "+mysql.escape(req.query.from_date)+" and "+ mysql.escape(req.query.to_date)+" )or (leaves.to_date between "+mysql.escape(req.query.from_date)+" and "+ mysql.escape(req.query.to_date)+"))"
+            if(req.query.status){
+                queryString+=" and approval_status="+ req.query.status
+            }
+            database.query(queryString , (err, leavesData, fields) => {
+                console.log(err)
+                res.send(leavesData) 
+                    
+            })
+        }
+
+
+
+    })
+
+}
+module.exports=getTotalLeaves

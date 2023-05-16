af249c48500e1a06df21d1aae9b3161465fbb4f1
wdAres <puneetsri9990@gmail.com>
2023-05-16 17:19:05 +0530

    Update getFines

--- api/modules/fines/getFines.js
+++ api/modules/fines/getFines.js
@@ -4,7 +4,7 @@ const getFines = (req, res, next) => {
     const role_id = req.body.result.role_id
     database.query("Select * from roles where id=" + role_id, (err, result) => {
         if (err) console.log(err)
-        let allowed_roles = ['HR Head','HR Assistant']
+        let allowed_roles = ['HR Head','HR Assistant','Super Admin','Admin']
         if (allowed_roles.includes(result[0].role_name)) {
             let queryString="SELECT job_details.*,employees.*, file_upload.name as photo, fines.*,employees.name as employee_name ,employees.employee_id as empID, floors.name as floor_name,stores.name as store_name from fines left join employees on employees.id=fines.employee_id left join job_details on job_details.id=employees.job_details_id left join floors on floors.id=job_details.floor_id left join file_upload on file_upload.id=employees.photo_id left join roles on job_details.role_id=roles.id left join stores on job_details.store_id=stores.id where fines.date>="+mysql.escape(req.query.from_date)+"and fines.date< "+mysql.escape(req.query.to_date)
             if(req.query.floor_name){

e3d055a14281e1f090119df26b1367ee16c18be2
anki356 <anki356@gmail.com>
2023-06-16 11:49:25 +0530

    Some minor Changes

--- api/modules/roles/addRole.js
+++ api/modules/roles/addRole.js
@@ -6,9 +6,68 @@ const addRole = (req, res, next) => {
         if (err) console.log(err)
         let allowed_roles = ['Super Admin']
         if (allowed_roles.includes(result[0].role_name)) {
+            
             database.query("Insert into roles (role_name,floor_id,store_id) values("+mysql.escape(req.body.role_name)+","+req.body.floor_id+","+req.body.store_id+")", (err, roleData, fields) => {
                 console.log(err)
+                let pr={}
+            if(req.body.attendance===true){
+pr.pr=new Promise((resolve,reject)=>{
+    pr.resolve=resolve
+    pr.reject=reject
+})
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+1+")",(err,permissionata)=>{
+pr.resolve(true)
+                })
+            }
+            if(req.body.timing_approval===true){
+                pr.pr=new Promise((resolve,reject)=>{
+                    pr.resolve=resolve
+                    pr.reject=reject
+                })
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+2+")",(err,permissionata)=>{
+                    pr.resolve(true)
+                                    })
+            }
+            if(req.body.interview===true){
+                pr.pr=new Promise((resolve,reject)=>{
+                    pr.resolve=resolve
+                    pr.reject=reject
+                })
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+3+")",(err,permissionata)=>{
+                    pr.resolve(true)
+                                    })
+            }
+            if(req.body.employee_detail===true){
+                pr.pr=new Promise((resolve,reject)=>{
+                    pr.resolve=resolve
+                    pr.reject=reject
+                })
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+4+")",(err,permissionata)=>{
+                    pr.resolve(true)
+                                    })
+            }
+            if(req.body.salary===true){
+                pr.pr=new Promise((resolve,reject)=>{
+                    pr.resolve=resolve
+                    pr.reject=reject
+                })
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+5+")",(err,permissionata)=>{
+                    pr.resolve(true)
+                                    })
+            }
+            if(req.body.fine_management===true){
+                pr.pr=new Promise((resolve,reject)=>{
+                    pr.resolve=resolve
+                    pr.reject=reject
+                })
+                database.query("Insert into permission_roles (role_id,permission_id) values("+roleData[0].id+","+6+")",(err,permissionata)=>{
+                    pr.resolve(true)
+                                    })
+            }
+            Promise.all([pr.pr]).then(()=>{
+
                 res.send(roleData) 
+            })
                     
             })
         }

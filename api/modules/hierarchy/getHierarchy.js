cbd68a16b36e96452489d38d2a662923055956db
anki356 <anki356@gmail.com>
2023-06-20 15:53:50 +0530

    Get Hierarchy

--- api/modules/hierarchy/getHierarchy.js
+++ api/modules/hierarchy/getHierarchy.js
@@ -1,14 +1,74 @@
 const database = require("../../../config/database");
 const mysql = require("mysql")
-const getHierarchy = (req, res, next) => {
+const getHierarchy =(req, res, next) => {
     const role_id = req.body.result.role_id
-    database.query("Select * from roles where id=" + role_id, (err, result) => {
+    database.query("Select * from roles where id=" + role_id,async (err, result) => {
         if (err) console.log(err)
         let allowed_roles = ['HR Head', 'Admin', 'Super Admin']
         if (allowed_roles.includes(result[0].role_name)) {
-database.query("select * from hierarchy where parent_role_id="+req.query.role_id,(err,hierarchyData)=>{
-    res.send(hierarchyData)
-})
+         function fetchTreeData(callback) {
+                const query = 'SELECT *, role_a.role_name as child_name,role_b.role_name as parent_name FROM hierarchy left join roles as role_a on role_a.id=hierarchy.child_role_id left join roles as role_b on role_b.id=hierarchy.parent_role_id ';
+                database.query(query, (err, rows) => {
+                  if (err) throw err;
+                  callback(rows);
+                });
+              }
+              async function fetchEmployeeData(value) {
+                return new Promise((resolve, reject) => {
+                  const query =
+                    "select * from employees left join job_details on job_details.id=employees.job_details_id where role_id="+value;
+                  database.query(query, (err, rows) => {
+                    if (err) reject(err);
+                    resolve(rows);
+                  });
+                });
+              }
+              fetchTreeData(async(treeData) => {
+                const treeArray = await traverseTree(treeData);
+//                 var promiseArray=[]
+//                 treeArray.forEach((data,index)=>{
+//                     let promise={}
+//                     promise.pr=new Promise((resolve,reject)=>{
+//                         promise.resolve=resolve
+//                         promise.reject=reject
+//                     })
+//                     promiseArray[index]=promise.pr
+                    
+// data['employees']=fetchEmployeeData(data.value)
+// promise.resolve(true)
+//                     })
+//                 })
+//                 Promise.all(promiseArray).then(()=>{
+
+                    res.json(treeArray);
+                // })
+
+              });
+              
+              async function traverseTree(nodes, parent_role_id = null) {
+                const result = [];
+                const filteredNodes = nodes.filter((node) => node.parent_role_id === parent_role_id);
+              
+                for (const node of filteredNodes) {
+                  const newNode = {
+                    value: node.child_role_id,
+                    role_name: node.child_name,
+                    children: await traverseTree(nodes, node.child_role_id),
+                    employees: await fetchEmployeeData(node.child_role_id)
+                  };
+                  
+                  // Perform desired operations on the node
+                  
+                  result.push(newNode);
+                }
+                
+                return result;
+              }
+             
+
+                 
+              
+              
         }
     })
 }

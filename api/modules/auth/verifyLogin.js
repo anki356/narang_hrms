86df0c2c3bca6d41e5f9a02b4e5f53763a0b6512
anki356 <39798400+anki356@users.noreply.github.com>
2023-08-10 17:10:01 +0530

    host corrected

--- api/modules/auth/verifyLogin.js
+++ api/modules/auth/verifyLogin.js
@@ -5,49 +5,52 @@ var jwt = require('jsonwebtoken');
 const verifyLogin = (req, res, next) => {
 
 
-
-    database.connect(() => {
-        database.query("SELECT * from users WHERE username = " + mysql.escape(req.body.username), (err, userResult, fields) => {
-            console.log(userResult)
-
-            console.log("err",err);
-
-            if (userResult?.length === 0) {
-
-
-                res.json({
-                    statusCode: 401,
-                    error: "Username is in correct"
-                })
-
-            }
-            else {
-
-                bcrypt.compare(req.body.password, userResult[0].password, function (err, result) {
-                    if (!result) res.json({
+    database.connect((err) => {
+        console.log(err)
+        if(!err){
+            database.query("SELECT * from users WHERE username = " + mysql.escape(req.body.username), (err, userResult, fields) => {
+                console.log(userResult)
+    
+                console.log("err",err);
+    
+                if (userResult?.length === 0) {
+    
+    
+                    res.json({
                         statusCode: 401,
-                        error: "Password is incorrect"
+                        error: "Username is in correct"
                     })
-                    else {
-
-                        var token = jwt.sign({
-                            data: {
+    
+                }
+                else {
+    
+                    bcrypt.compare(req.body.password, userResult[0].password, function (err, result) {
+                        if (!result) res.json({
+                            statusCode: 401,
+                            error: "Password is incorrect"
+                        })
+                        else {
+    
+                            var token = jwt.sign({
+                                data: {
+                                    username: userResult[0].username,
+                                    role_id: userResult[0].role_id
+                                }
+                            }, 'secret');
+                            res.json({
+                                token: token,
                                 username: userResult[0].username,
                                 role_id: userResult[0].role_id
-                            }
-                        }, 'secret');
-                        res.json({
-                            token: token,
-                            username: userResult[0].username,
-                            role_id: userResult[0].role_id
-                        })
-                    }
-
-                })
-
-            }
-        })
-        
+                            })
+                        }
+    
+                    })
+    
+                }
+            })
+            
+        }
+   
     })
 
 
